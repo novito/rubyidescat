df97b23c3fcb82154981ce31c56138e0ee3c24a9
Joan Roig <roigarderiu@gmail.com>
2013-11-11 21:54:43 -0800

    removed indicadors spec and lib

--- lib/rubyidescat/indicadors.rb
+++
@@ -1,7 +0,0 @@
-module Rubyidescat
-  class Indicadors << Rubyidescat::Client
-    def initialize version, operation, format, *args
-      
-    end
-  end
-end
--- spec/indicadors_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-module Rubyidescat::Indicadors
-  describe 'Indicadors Search' do
-    let(:client) { Rubyidescat::Client.new }
-  end
-end
